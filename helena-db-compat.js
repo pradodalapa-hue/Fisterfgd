9c5283bf9d100164b20cf04933655de673d38b47
pradodalapa-hue <pradodalapa@gmail.com>
2026-07-27 20:19:40 -0400

    HELENA OPERATOR: Atualizado helena-db-compat.js

--- helena-db-compat.js
+++ helena-db-compat.js
@@ -2,11 +2,44 @@
 (function() {
     "use strict";
 
-    console.log("%c[HELENA ENGINE]: Inicializando conexões soberanas com jdpsistemas.com.br...", "color: #06b6d4; font-weight: bold;");
+    console.log("%c[HELENA CORE]: Inicializando motor soberano de rede em jdpsistemas.com.br...", "color: #06b6d4; font-weight: bold;");
+
+    // Algoritmo de geração de ID único cronológico (evita colisões de rede)
+    const PUSH_CHARS = '-0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz';
+    let lastPushTime = 0;
+    let lastRandChars = [];
+
+    function generatePushId() {
+        let now = new Date().getTime();
+        let duplicateTime = (now === lastPushTime);
+        lastPushTime = now;
+        let timeStampChars = new Array(8);
+        for (let i = 7; i >= 0; i--) {
+            timeStampChars[i] = PUSH_CHARS.charAt(now % 64);
+            now = Math.floor(now / 64);
+        }
+        let id = timeStampChars.join('');
+        if (!duplicateTime) {
+            for (let i = 0; i < 12; i++) {
+                lastRandChars[i] = Math.floor(Math.random() * 64);
+            }
+        } else {
+            let i;
+            for (i = 11; i >= 0 && lastRandChars[i] === 63; i--) {
+                lastRandChars[i] = 0;
+            }
+            lastRandChars[i]++;
+        }
+        for (let i = 0; i < 12; i++) {
+            id += PUSH_CHARS.charAt(lastRandChars[i]);
+        }
+        return id;
+    }
 
     class Snapshot {
-        constructor(value) {
+        constructor(value, key) {
             this._value = value;
+            this.key = key;
         }
         val() {
             return this._value;
@@ -14,16 +47,28 @@
         exists() {
             return this._value !== null && this._value !== undefined;
         }
+        forEach(callback) {
+            if (this._value && typeof this._value === 'object') {
+                Object.keys(this._value).forEach(k => {
+                    callback(new Snapshot(this._value[k], k));
+                });
+            }
+        }
     }
 
     class Reference {
         constructor(path, db) {
-            this.path = path.replace(/^\/|\/$/g, ''); // Normaliza barras
+            this.path = path.replace(/^\/|\/$/g, ''); 
             this.db = db;
         }
 
+        get key() {
+            return this.path.split('/').pop() || null;
+        }
+
         child(subPath) {
-            return new Reference(this.path + '/' + subPath.replace(/^\/|\/$/g, ''), this.db);
+            const cleanSub = subPath.replace(/^\/|\/$/g, '');
+            return new Reference(this.path ? `${this.path}/${cleanSub}` : cleanSub, this.db);
         }
 
         once(eventType) {
@@ -31,7 +76,7 @@
                 if (eventType === 'value') {
                     const reqId = Math.random().toString(36).substring(2, 15);
                     this.db.pendingRequests[reqId] = (data) => {
-                        resolve(new Snapshot(data));
+                        resolve(new Snapshot(data, this.key));
                     };
                     this.db.send({
                         action: 'ONCE',
@@ -55,7 +100,7 @@
             }
         }
 
-        off(eventType) {
+        off() {
             this.db.send({
                 action: 'UNSUBSCRIBE',
                 path: this.path
@@ -78,22 +123,31 @@
             return this.set(null);
         }
 
+        push(value) {
+            const newKey = generatePushId();
+            const newRef = this.child(newKey);
+            if (value !== undefined) {
+                newRef.set(value);
+            }
+            return newRef;
+        }
+
         transaction(updateFn, onComplete) {
             this.once('value').then(snap => {
                 const currentVal = snap.val();
                 const newVal = updateFn(currentVal);
                 this.set(newVal).then(() => {
                     if (onComplete) {
-                        onComplete(null, true, new Snapshot(newVal));
+                        onComplete(null, true, new Snapshot(newVal, this.key));
                     }
                 });
             });
         }
     }
 
-    class MockDatabase {
+    class HelenaDatabase {
         constructor(databaseURL) {
-            // Se nenhuma URL for passada, conecta automaticamente via SSL seguro no seu domínio
+            // Conexão direta ao barramento WSS seguro em jdpsistemas.com.br
             this.url = databaseURL || "wss://jdpsistemas.com.br";
             this.listeners = {};
             this.pendingRequests = {};
@@ -105,9 +159,9 @@
             this.socket = new WebSocket(this.url);
 
             this.socket.onopen = () => {
-                console.log("[HELENA-DB]: Canal de comunicação seguro estabelecido.");
+                console.log("[HELENA-DB]: Conexão estabelecida com jdpsistemas.com.br.");
                 
-                // Descarrega o buffer de operações offline
+                // Descarrega o buffer de dados pendentes acumulados offline
                 while (this.queue.length > 0) {
                     this.socket.send(JSON.stringify(this.queue.shift()));
                 }
@@ -124,7 +178,7 @@
                     if (packet.action === 'SYNC') {
                         const path = packet.path;
                         if (this.listeners[path]) {
-                            const snap = new Snapshot(packet.value);
+                            const snap = new Snapshot(packet.value, path.split('/').pop());
                             this.listeners[path].forEach(callback => callback(snap));
                         }
                     }
@@ -142,7 +196,7 @@
             };
 
             this.socket.onclose = () => {
-                console.log("[HELENA-DB] Conexão encerrada pelo host. Reconectando em 3s...");
+                console.log("[HELENA-DB] Conexão encerrada. Reconectando em 3s...");
                 if (window.onHelenaStatusChange) {
                     window.onHelenaStatusChange(false);
                 }
@@ -154,19 +208,19 @@
             if (this.socket && this.socket.readyState === WebSocket.OPEN) {
                 this.socket.send(JSON.stringify(data));
             } else {
-                this.queue.push(data); // Armazena em cache offline se o celular perder o sinal
+                this.queue.push(data); // Buffer de segurança offline
             }
         }
 
         ref(path) {
-            return new Reference(path, this);
+            return new Reference(path || "", this);
         }
     }
 
-    // Define o objeto global exatamente igual ao Firebase para manter compatibilidade absoluta
+    // Exportação da API Unificada
     window.firebase = {
         initializeApp: function(config) {
-            this._db = new MockDatabase(config.databaseURL);
+            this._db = new HelenaDatabase(config.databaseURL);
             return this;
         },
         database: function() {
